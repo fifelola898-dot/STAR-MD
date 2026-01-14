6b138af313170eb85d3f2960507c2a3891b08dd4
laban391 <laviaarori@gmail.com>
2026-01-14 21:12:32 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'STAR MD',
+  author: 'GAAJU',
+  botName: "STAR MD",
+  botOwner: 'CHRIS GAAJU', // Your name
+  ownerNumber: '2348069675806', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.0.0",
+  updateZipUrl: "https://github.com/Xchristech2/STAR-MD/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
